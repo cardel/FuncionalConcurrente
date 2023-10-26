3c03b571bda74b030f4fc854c18002dae20b5836
Carlos Delgado <cardel87@gmail.com>
2023-10-26 13:44:46 -0500

    26 de oct

--- 2023-2/jueves/8-Colecciones/Primos.scala
+++ 2023-2/jueves/8-Colecciones/Primos.scala
@@ -21,11 +21,16 @@ object Primos{
     val res = valores.map(n % _)
     res exists (_ == 0)
   }
+  def generador(n:Int):List[(Int,Int)] = {
+    //r flatMap (x => x+1 to 10 map (y => (y,x)) 
+    ((1 to n) flatMap (x => x + 1 to n map (y => (y,x)) filter ({case (a,b) => primos(a+b)})  )).toList
+  }
   def main(args: Array[String]): Unit = {
     val valores = 1 to 100
     println("Numeros primos del 1 al 100 "+valores.filter(primos(_)))
     println("Numeros primos del 1 al 100 "+valores.filter(primosB(_)))
     println("Numeros compueestos del 1 al 100"+valores.filter(compuesto(_)))
+    println(generador(10))
   }
   
 }
